16278fa874ed56b8c25d18fa1c82ad8771745b12
Waleed <wa318536497@gmail.com>
2022-04-05 16:15:26 +0300

    reset search input upon submit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
   const HandleSearchSubmit = (e)=>{ 
     e.preventDefault();
     console.log(word);
-    fetch(`https://api.unsplash.com//photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
     .then((res) =>res.json())
     .then((data) => {
       console.log(data);
@@ -20,6 +20,7 @@ function App() {
     .catch((err) =>{
       console.log(err);
     })
+    setWord('');
   }
   
   return (
