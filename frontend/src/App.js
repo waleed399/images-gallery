28be88b655a588e5cd1ab79ba0120079b23a224f
Waleed <wa318536497@gmail.com>
2022-04-07 16:49:48 +0300

    display and delete images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,50 @@
-import React from 'react'
-import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import Header from './components/Header';
-import Search from './components/Search';
+import React from "react";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import ImageCard from "./components/ImageCard";
+import { Container, Row, Col } from "react-bootstrap";
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
-  const [word,setWord] = useState('');
-  const[images,SetImages] = useState([]);
+  const [word, setWord] = useState("");
+  const [images, SetImages] = useState([]);
 
-  console.log(images);
-
-  const HandleSearchSubmit = (e)=>{ 
+  const HandleSearchSubmit = (e) => {
     e.preventDefault();
-    console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
-    .then((res) =>res.json())
-    .then((data) => {
-      console.log(data);
-      SetImages([data,...images]);
-     
-    })
-    .catch((err) =>{
-      console.log(err);
-    })
-    setWord('');
-  }
-  
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        SetImages([{ ...data, title: word }, ...images]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setWord("");
+  };
+
+  const HandleDeleteImage = (id)=>{
+    SetImages(images.filter((image)=> image.id!==id));
+  };
+
   return (
     <div>
-      <Header title="Images Gallery"/>
-      <Search word={word} setWord={setWord} HandleSubmit={HandleSearchSubmit}/>
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} HandleSubmit={HandleSearchSubmit} />
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, i) => (
+            <Col key={i} className = "pb-3">
+              <ImageCard image={image} deleteImage={HandleDeleteImage}/>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 }
