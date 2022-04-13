1318d968cce97387de2f2b77b7e0cbade8151ae0
Waleed <wa318536497@gmail.com>
2022-04-13 21:39:04 +0300

    sample flask app

--- api/main.py
+++ api/main.py
@@ -1,1 +1,12 @@
-print("HO")
+# save this as app.py
+from platform import freedesktop_os_release
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5050)
