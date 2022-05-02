52758ad6ededa593b3c9a0f491313ea9a676e3c4
Waleed <wa318536497@gmail.com>
2022-05-02 22:58:44 +0300

    image athor info

--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -1,6 +1,10 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button} from "react-bootstrap";
+import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
+import { Nav } from "react-bootstrap";
 const ImageCard = ({ image, deleteImage, saveImage }) => {
+  const authorName = image.user.name || "no author name"
+  const authorPortfolioURL = image.user && image.user.portfolio_url
   return (
     <Card style={{ width: "18rem" }}>
       <Card.Img variant="top" src={image.urls.small} />
@@ -13,6 +17,12 @@ const ImageCard = ({ image, deleteImage, saveImage }) => {
           Save
           </Button>}
       </Card.Body>
+      <Card.Footer className="text-center text-muted">
+        {authorPortfolioURL && (
+        <Nav.Link href={authorPortfolioURL} target="_blanc">{authorName}</Nav.Link>
+        )}
+        {!authorPortfolioURL && authorName}
+      </Card.Footer>
     </Card>
   );
 };
