2f6048872cc655baee3026436af9c60bf2ffccb1
Waleed <wa318536497@gmail.com>
2022-05-07 21:16:48 +0300

    Full Stack Course Finished !!!!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
-import axios from 'axios';
-import { useState,useEffect } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { ToastContainer ,toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import "bootstrap/dist/css/bootstrap.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
@@ -9,92 +11,107 @@ import { Container, Row, Col } from "react-bootstrap";
 import Welcome from "./components/welcome";
 import Spinner from "./components/Spinner";
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5050'
+const API_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:5050";
 
 function App() {
   const [word, setWord] = useState("");
   const [images, SetImages] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const getSavedImages = async () =>{
+  const getSavedImages = async () => {
     try {
-      const res = await axios.get(`${API_URL}/images`)
+      const res = await axios.get(`${API_URL}/images`);
       SetImages(res.data || []);
       setLoading(false);
+      if(res.data.length){toast("Saved images downloaded successfully !!!");}
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
-  useEffect(() => getSavedImages(), [])
+  useEffect(() => getSavedImages(), []);
 
   const HandleSearchSubmit = async (e) => {
     e.preventDefault();
-   
+
     try {
       const res = await axios.get(`${API_URL}/new-image?query=${word}`);
       SetImages([{ ...res.data, title: word }, ...images]);
+      toast.info(`New Image ${word.toUpperCase()} was found`);
     } catch (error) {
       console.log(error);
+      toast.error(error.message)
     }
 
     setWord("");
   };
 
-  const handleSaveImage = async (id) =>{
+  const handleSaveImage = async (id) => {
     const imageToSave = images.find((image) => image.id === id);
     imageToSave.saved = true;
 
     try {
       const res = await axios.post(`${API_URL}/images`, imageToSave);
-      if(res.data && res.data.inserted_id) {
-        SetImages(images.map((image) => 
-        image.id === id ? {...image, saved: true} : image));
+      if (res.data && res.data.inserted_id) {
+        SetImages(
+          images.map((image) =>
+            image.id === id ? { ...image, saved: true } : image
+          )
+        );
       }
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  const HandleDeleteImage = async (id) => {
-    
-    try {
-      const res = await axios.delete(`${API_URL}/images/${id}`);
-      
-      if (res.data && res.data.deleted_id) {
-        
-        SetImages(images.filter((image) => image.id!==id))
-      }
+      toast.success("Image was saved to DB successfully !!!");
     } catch (error) {
       console.log(error);
+      toast.error(error.message)
     }
-    
+  };
+
+  const HandleDeleteImage = async (id) => {
+    try {
+      const res = await axios.delete(`${API_URL}/images/${id}`);
+
+      if (res.data && res.data.deleted_id) {
+        SetImages(images.filter((image) => image.id !== id));
+      }
+      toast.warning(`Image ${images.find((i) => i.id ===id).title.toUpperCase()} was deleted from DB`);
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
   };
 
   return (
     <div>
       <Header title="Images Gallery" />
-      {loading ? <Spinner /> : <>
-      <Search word={word} setWord={setWord} HandleSubmit={HandleSearchSubmit} />
-      <Container className="mt-4">
-        {images.length ? (
-          <Row xs={1} md={2} lg={3}>
-            {images.map((image, i) => (
-              <Col key={i} className="pb-3">
-                <ImageCard image={image} 
-                deleteImage={HandleDeleteImage} 
-                saveImage={handleSaveImage}/>
-              </Col>
-            ))}
-          </Row>
-        ) : (
-          <Welcome />
-        )}
-      </Container></>}
-      
-      
-    </div>
-  );
-}
-
-export default App;
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Search
+            word={word}
+            setWord={setWord}
+            HandleSubmit={HandleSearchSubmit}
+          />
+          <Container className="mt-4">
+            {images.length ? (
+              <Row xs={1} md={2} lg={3}>
+                {images.map((image, i) => (
+                  <Col key={i} className="pb-3">
+                    <ImageCard
+                      image={image}
+                      deleteImage={HandleDeleteImage}
+                      saveImage={handleSaveImage}
+                    />
+                  </Col>
+                ))}
+              </Row>
+            ) : (
+              <Welcome />
+            )}
+          </Container>
+        </>
+      )}
+      <ToastContainer position="bottom-right" />
+</div>
+  )}
+  export default App;
